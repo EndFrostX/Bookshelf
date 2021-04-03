555b92973ac4d873b733850884c1a5df3853fc1a
Rithiya Vibol <vibolrithiya@gmail.com>
2021-04-03 10:15:21 +0700

    testing category drop down

--- lib/Pages/Create.dart
+++ lib/Pages/Create.dart
@@ -1,6 +1,8 @@
+import 'package:bookshelf/Models/BookCategory.dart';
+import 'package:bookshelf/Repos/book_category_repo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:searchable_dropdown/searchable_dropdown.dart';
+import 'package:searchable_dropdown/searchable_dropdown.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -9,31 +11,16 @@ class CreatePage extends StatefulWidget {
 
 class _CreatePageState extends State<CreatePage> {
   double _width;
-
+  List<BookCategory> _data;
   var _pdfController = TextEditingController();
   var _titleController = TextEditingController();
   var _authorController = TextEditingController();
   var _descriptionController = TextEditingController();
-  var _categoryController = TextEditingController();
+  var _categoryController;
 
-  Map<String, Widget> _widgets;
 
   @override
   Widget build(BuildContext context) {
-    // _widgets = {
-    //   "Single dialog": SearchableDropdown.single(
-    //     items: items,
-    //     value: selectedValue,
-    //     hint: "Select one",
-    //     searchHint: "Select one",
-    //     onChanged: (value) {
-    //       setState(() {
-    //         selectedValue = value;
-    //       });
-    //     },
-    //     isExpanded: true,
-    //   ),
-    // }
     _width = MediaQuery
         .of(context)
         .size
@@ -49,7 +36,46 @@ class _CreatePageState extends State<CreatePage> {
       title: Text("Upload Book"),
     );
   }
-
+  get _futureBuilder{
+    return FutureBuilder(
+        future: getAllBookCategories(),
+        builder: (context, snapshot){
+      if(snapshot.connectionState == ConnectionState.done){
+        _data = snapshot.data.data;
+        return _contentCategory();
+      }
+      else{
+        return CircularProgressIndicator();
+      }
+    });
+  }
+  _contentCategory(){
+    return Card(
+      child: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(27)),
+        child: Column(
+          children: [
+            Text("Enter"),
+            SearchableDropdown.single(
+              items: _data.map((category){
+                return new DropdownMenuItem(child: Text(category.name), value: category.name,);
+              }).toList(),
+              isExpanded: true,
+              value: _categoryController,
+              searchHint: new Text("Select"),
+              label: Text("Category"),
+              onChanged: (value){
+                setState(() {
+                  _categoryController = value;
+                  print(_categoryController);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
   get _myBody {
     return SingleChildScrollView(
       child: Column(
@@ -72,11 +98,12 @@ class _CreatePageState extends State<CreatePage> {
 
           _textFieldStuff("Description", "Enter the description",
               capitalization: TextCapitalization.sentences,
-              myController: _descriptionController),
-
-          _textFieldStuff("Category", "Enter the description",
-              capitalization: TextCapitalization.sentences,
-              myController: _categoryController),
+              myController: _descriptionController,
+              max: 10),
+          _futureBuilder,
+          // _futureBuilder,
+          ElevatedButton(child: Text("Upload"), onPressed: (){
+          },)
         ],
       ),
     );
@@ -85,13 +112,15 @@ class _CreatePageState extends State<CreatePage> {
   _textFieldStuff(String label, String hint,
        {TextInputType keyboard = TextInputType.text,
         TextCapitalization capitalization = TextCapitalization.none,
-        TextEditingController myController}) {
+        TextEditingController myController,int min=1, int max=2}) {
     return Container(
-      width: _width * 0.90,
+      width: _width * 0.80,
       child: Column(
         children: [
           TextField(
             controller: myController,
+            maxLines: max,
+            minLines: min,
             decoration: InputDecoration(
               labelText: label,
               hintText: hint,
--- lib/repos/book_category_repo.dart
+++ lib/repos/book_category_repo.dart
@@ -1,10 +1,8 @@
-import 'package:bookshelf/models/BookCategory.dart';
 import 'package:bookshelf/models/ServerResponse.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
 String address = "localhost:3000";
-
 Future<ServerResponse> getAllBookCategories() async {
   Uri url = Uri.http(address, "/book-categories");
   Response res = await get(url);
